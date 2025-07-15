d9e9a79f3638cd5ae93b38b602ddf8a85cfa12c7
hester999 <ivankuzin09@gmail.com>
2025-07-15 20:27:30 +0300

    final v1

--- src/server/internal/dto/message_dto.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type MessageDTO struct {
-	Type string `json:"type"`
-	Name string `json:"name"`
-	Text string `json:"text,omitempty"`
-	Time string `json:"time,omitempty"`
-	Dst  string `json:"dst,omitempty"`
-}
